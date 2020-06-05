e96fd5839c0661dc6f324d2df3287013269980d6
Ammon Leslie <ammonleslie@gmail.com>
2020-06-05 00:23:44 +0000

    Updated tests for testing city names instead of zip codes

--- tests/server/apiv1.spec.js
+++ tests/server/apiv1.spec.js
@@ -54,10 +54,10 @@ describe('Get Weather', function() {
    // assert(resMock.send.lastCall.calledWith('Failed to get the data'), 'Unexpected response:' + resMock.send.lastCall.args);
   });
 
-  it('with incomplete zip code', function() {
+  it('with incomplete name', function() {
     reqMock = {
       query: {
-        zip: 'El Paso'
+        zip: 'Hami'
       }
     };
 
@@ -73,16 +73,16 @@ describe('Get Weather', function() {
     assert(resMock.send.lastCall.args[0].msg === 'Failed', 'Unexpected response:' + resMock.send.lastCall.args);
   });
 
-  it('with valid zip code', function() {
+  it('with valid name', function() {
     reqMock = {
       query: {
-        zip: 'El Paso'
+        zip: 'Hamilton'
       }
     };
 
     const body = {
       cod: 200,
-      name: 'El Paso',
+      name: 'Hamilton',
       weather: [
         {
           main: 'cold'
@@ -102,7 +102,7 @@ describe('Get Weather', function() {
     apiv1.getWeather(reqMock, resMock);
 
     assert(resMock.status.lastCall.calledWith(200), 'Unexpected response:' + resMock.status.lastCall.args);
-    assert(resMock.send.lastCall.args[0].city === 'El Paso', 'Unexpected response:' + resMock.send.lastCall.args[0].city);
+    assert(resMock.send.lastCall.args[0].city === 'Hamilton', 'Unexpected response:' + resMock.send.lastCall.args[0].city);
     assert(resMock.send.lastCall.args[0].weather === 'Conditions are cold and temperature is 78 F', 'Unexpected response:' + resMock.send.lastCall.args[0].weather);
   });
 });
